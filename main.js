bf5f32bc600caa77ba366884fadf26ad6ff212d2
ParhamInBinart <parham.berenjian@gmail.com>
2022-12-10 22:23:43 +0100

    Added eventListeners

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import { startTimeAndDate } from "./JS-files/startTimeAndDate.js";
 import { loadCalendarDays } from "./JS-files/loadCalendarDays.js";
+import { goToPrevMonth } from "./JS-files/switchMonth.js";
+import { goToNextMonth } from "./JS-files/switchMonth.js";
 
 main();
 
@@ -9,4 +11,13 @@ main();
 function main() {
     startTimeAndDate();
     loadCalendarDays();
+    addEventListeners();
+}
+
+/**
+ * EventListeners thoughout the script.
+ */
+function addEventListeners() {
+    document.querySelector(".fa-angle-left").addEventListener("click", goToPrevMonth);
+    document.querySelector(".fa-angle-right").addEventListener("click", goToNextMonth);
 }

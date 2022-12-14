4b0773a226cbe50445e18d0363ad6bbcdace7d73
ParhamInBinart <parham.berenjian@gmail.com>
2022-12-14 01:42:50 +0100

    remade functions

--- JS-files/eventItem.js
+++ JS-files/eventItem.js
@@ -1,22 +1,21 @@
+const eventTitle = document.querySelector("#title");
+const eventDesc = document.querySelector("#description");
+const eventOccasion = document.querySelector("#occasion");
+const eventMonth = document.querySelector("#choseMonth");
+const eventDay = document.querySelector("#choseDay");
+const eventYear = document.querySelector("#choseYear");
 
-export function addEventItem() {
-    const inputs = document.querySelectorAll(".addEventWindow input, .addEventWindow textarea, .addEventWindow select");
-    for (const input of inputs) {
-      input.addEventListener("input", saveToLS);
+
+export function addEventToLS() {
+    const date = `${eventYear.value}-${eventMonth.value}-${eventDay.value}`;
+
+    const eventItem = {
+        title : eventTitle.value,
+        date,
+        description : eventDesc.value,
+        occasion : eventOccasion.value,
     }
-}
+    localStorage.setItem("events", JSON.stringify(eventItem))
 
-function saveToLS(event) {
-    const input = event.target;
-    localStorage.setItem(input.name, input.value);
-}
-
-export function addEventToList() {
-    console.log("ASd")
-    const eventItem = document.querySelector(".eventItem");
-    const eventItemTitle = document.querySelector(".eventItemtitle");
-    const eventItemDesc = document.querySelector(".eventItemDescription");
-    const eventItemDate = document.querySelector(".eventItemDate");
-
-    eventItem.classList.add(".eventItem");
+    console.log(eventItem)
 }

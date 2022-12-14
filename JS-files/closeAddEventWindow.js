17eacbe96b86686b178720d4d10cf4a0ed6d0ba7
ParhamInBinart <parham.berenjian@gmail.com>
2022-12-14 15:56:46 +0100

    added file for closing add event window

---
+++ JS-files/closeAddEventWindow.js
@@ -0,0 +1,11 @@
+
+export function closeAddEventWindow() {
+    document.querySelector(".dropdownWindow").classList.toggle("displayNone");
+    document.querySelector(".blurCover").classList.toggle("displayBlock");
+    document.querySelector(".addEventWindow").classList.toggle("displayFlex");
+
+    const inputs = document.querySelectorAll("input, textarea, select");
+    for (const input of inputs) {
+      input.value = "";
+    }
+}

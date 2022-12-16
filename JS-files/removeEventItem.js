d481c32f6d959002ae8ad8e17004df47f77e9d1b
ParhamInBinart <parham.berenjian@gmail.com>
2022-12-16 19:55:18 +0100

    removed exess file

--- JS-files/removeEventItem.js
+++
@@ -1,6 +0,0 @@
-
-// export function removeEventItem() {
-//     console.log("asd")
-//     const eventItem = document.querySelector(".eventItem");
-//     eventItem.classList.remove("eventItem");
-// }

766c196d955fb295e9a7f982b73e37f40da98643
ParhamInBinart <parham.berenjian@gmail.com>
2022-12-17 19:08:36 +0100

    tweaked function of buttons displayed

--- JS-files/closeAddEventWindow.js
+++ JS-files/closeAddEventWindow.js
@@ -1,7 +1,10 @@
 export function closeAddEventWindow() {
-  document.querySelector(".dropdownWindow").classList.toggle("displayNone");
-  document.querySelector(".blurCover").classList.toggle("displayBlock");
-  document.querySelector(".addEventWindow").classList.toggle("displayFlex");
+  document.querySelector(".dropdownWindow").classList.remove("displayNone");
+  document.querySelector(".blurCover").classList.remove("displayBlock");
+  document.querySelector(".addEventWindow").classList.remove("displayFlex");
+  document.querySelector("#submitEvent").classList.remove("displayFlex");
+  const saveChanges = document.querySelector("#saveChanges");
+  saveChanges.classList.remove("displayFlex");
 
   const inputs = document.querySelectorAll("input, textarea, select");
   for (const input of inputs) {

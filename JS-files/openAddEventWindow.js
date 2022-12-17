cd69655c328d0d50d19d8e23600e44282bcae0ee
ParhamInBinart <parham.berenjian@gmail.com>
2022-12-17 13:58:18 +0100

    displaying and hiding button

--- JS-files/openAddEventWindow.js
+++ JS-files/openAddEventWindow.js
@@ -5,4 +5,7 @@ export function openAddEventWindow() {
   document.querySelector(".dropdownWindow").classList.toggle("displayNone");
   document.querySelector(".blurCover").classList.toggle("displayBlock");
   document.querySelector(".addEventWindow").classList.toggle("displayFlex");
+  document.querySelector("#submitEvent").classList.toggle("displayFlex");
+  document.querySelector("#saveChanges").classList.toggle("displayNone");
+
 }

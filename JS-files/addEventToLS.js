f56231bc96cadf0a75b69328cfb1a5d9bdb19883
ParhamInBinart <parham.berenjian@gmail.com>
2022-12-17 13:57:40 +0100

    -

--- JS-files/addEventToLS.js
+++ JS-files/addEventToLS.js
@@ -1,9 +1,10 @@
-const eventTitleInput = document.querySelector("#title");
-const eventDescInput = document.querySelector("#description");
-const eventOccasionInput = document.querySelector("#occasion");
-const eventDateInput = document.querySelector("#pickDate");
 
 export function addEventToLS() {
+  const eventTitleInput = document.querySelector("#title");
+  const eventDescInput = document.querySelector("#description");
+  const eventOccasionInput = document.querySelector("#occasion");
+  const eventDateInput = document.querySelector("#pickDate");
+  
   const events = JSON.parse(localStorage.getItem('events')) || [];
 
   const date = eventDateInput.value;
